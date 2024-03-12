ca9465c6202df605675e4e02c5319777988864ba
Maurine Karanja <kmauryn14@gmail.com>
2024-03-12 11:11:48 +0300

    initial commit

---
+++ Day1.py
@@ -0,0 +1,22 @@
+#Variables
+name = 'Dan'
+
+NAME = 'Dan'
+
+print(name)
+#Python uses snake_case....it can also use camelCase.
+
+
+#Data Types
+#Primitive
+#String -> ""or ''
+#number -> int, float
+#Boolean -> True,False
+#None
+
+
+#Non Primitive
+#List
+#Tuple
+#Set
+#Dictionary
